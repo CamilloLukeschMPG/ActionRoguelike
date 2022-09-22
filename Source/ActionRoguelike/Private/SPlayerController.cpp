711a2ad668ddc4a4d732dd08ef57a5c9493bee3f
mpgcamluk <camillolukesch@mpg.io>
2022-09-22 19:12:50 +0200

    Lecture 24 done

--- Source/ActionRoguelike/Private/SPlayerController.cpp
+++ Source/ActionRoguelike/Private/SPlayerController.cpp
@@ -3,10 +3,44 @@
 
 #include "SPlayerController.h"
 
+#include "Blueprint/UserWidget.h"
+
+
 void ASPlayerController::BeginPlayingState()
 {
 	//Super::BeginPlayingState();
 	BlueprintBeginPlayingState();
+
+}
+
+void ASPlayerController::TogglePauseMenu()
+{
+	if (PauseMenuInstance && PauseMenuInstance->IsInViewport())
+	{
+		PauseMenuInstance->RemoveFromParent();
+		PauseMenuInstance = nullptr;
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+		return;
+	}
+
+	PauseMenuInstance = CreateWidget<UUserWidget>(this, PauseMenuClass);
+
+	if (PauseMenuInstance)
+	{
+		PauseMenuInstance->AddToViewport(100);
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("PauseMenu", IE_Pressed, this, &ASPlayerController::TogglePauseMenu);
 }
 
 void ASPlayerController::SetPawn(APawn* InPawn)
@@ -15,3 +49,4 @@ void ASPlayerController::SetPawn(APawn* InPawn)
 
 	OnPawnChanged.Broadcast(InPawn);
 }
+
